f082463143e14230622d18b5e1fb6507162692db
linuxgurugamer@gmail.com <linuxgurugamer@gmail.com>
2024-07-24 10:56:41 -0400

    Fixes FIGS dll name Fixes deploy.bat for new directory Added version files to GAMEDATA dirs

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 
-@echo off
+rem @echo off
 
 rem H is the destination game folder
 rem GAMEDIR is the name of the mod folder (usually the mod name)
@@ -10,7 +10,7 @@ rem    but not always
 set H=%KSPDIR%
 set GAMEDIR=FruitKocktail
 set GAMEDATA="GameData"
-set VERSIONFILE=%GAMEDIR%.version
+set VERSIONFILE=%3\%3.version
 
 copy /Y "%1%2" "%GAMEDATA%\%GAMEDIR%\%3\Plugins"
 copy /Y "%1%3".pdb "%GAMEDATA%\%GAMEDIR%\Plugins"
@@ -18,4 +18,4 @@ copy /Y %VERSIONFILE% %GAMEDATA%\%GAMEDIR%
 
 IF EXIST %3\README.md copy /y  %3\README.md "%GAMEDATA%\%GAMEDIR%\%3"
 
-rem pause
+pause
