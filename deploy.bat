19affc148cdf533820be2f7f988154a04e81548d
linuxgurugamer@gmail.com <linuxgurugamer@gmail.com>
2024-07-24 11:07:28 -0400

    Put version file into correct location

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 
-rem @echo off
+ @echo off
 
 rem H is the destination game folder
 rem GAMEDIR is the name of the mod folder (usually the mod name)
@@ -14,8 +14,8 @@ set VERSIONFILE=%3\%3.version
 
 copy /Y "%1%2" "%GAMEDATA%\%GAMEDIR%\%3\Plugins"
 copy /Y "%1%3".pdb "%GAMEDATA%\%GAMEDIR%\Plugins"
-copy /Y %VERSIONFILE% %GAMEDATA%\%GAMEDIR%
+copy /Y %VERSIONFILE% %GAMEDATA%\%GAMEDIR%\%3
 
 IF EXIST %3\README.md copy /y  %3\README.md "%GAMEDATA%\%GAMEDIR%\%3"
 
-pause
+rem pause
